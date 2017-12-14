b1b06ca93da5d45561072b82df54d4fcfea82d93
Antonio Abraham <alopez@baktun.net>
2017-12-13 18:01:53 -0600

    finished all functionality

--- routes/company.js
+++ routes/company.js
@@ -1,48 +1,31 @@
 'use strict'
 
 const json2csv = require('json2csv');
-const csv = require('fast-csv');
-
-// const data = {
-//   avgCompanyIncome: '',
-//   avgEmployeeIncome:'',
-//   company: '',
-//   companyType: 'startup etc0',
-//   itEmployees:'',
-//   totalItEmployees: 0,
-//   jobPerks:'',
-//   officeCondition: '',
-//   avgEmployeeEmotionalStatus: 1,
-//   avgEmployeeSatisfactionLevel: 2,
-// }
+const csv = require('fast-csv')
 
 module.exports = (app, db) => {
   app.post('/company-data', (req, res) => {
-    console.log("aquimero");
     if (!req.files) {
-      console.log("here");
-      
       return res.status(400).send('Sin archivo')
     }
     
     const dataFile = req.files.file;
     const companyData = [];
-
-    console.log("hello");
-    console.log(dataFile);
-  
+         
     csv
      .fromString(dataFile.data.toString(), {
         headers: true,
         ignoreEmpty: true
      })
-     .on("data", (data) => {
+     .on("data", function(data){
         data['insertionDate'] = new Date()
+          
         companyData.push(data);
      })
-     .on("end", async () => {
+     .on("end", function(){
+        //mongoInsert
         try {
-          const result = await db.collection('CompanyData').insert(companyData)
+          const result = db.collection('CompanyData').insert(companyData)
           if (result) {
             res.send(`Se subieron ${companyData.length} registros con exito`);
           } 
@@ -53,7 +36,6 @@ module.exports = (app, db) => {
   })
 
   app.get('/company-data', async (req, res) => {
-    console.log("jajajajajja");
     try {
       const data = await db.collection('CompanyData').find({}).toArray()
       res.json(data)
@@ -64,27 +46,21 @@ module.exports = (app, db) => {
 
   app.get('/company-data/csv', async (req, res) => {
     try {
-      const data = await db.collection('CompanyData').find({}).toArray()
-      // const fields = [
-      //   'avgCompanyIncome',
-      //   'avgEmployeeIncome',
-      //   'company',
-      //   'companyType',
-      //   'itEmployees',
-      //   'totalItEmployees',
-      //   'jobPerks',
-      //   'officeCondition',
-      //   'avgEmployeeEmotionalStatus',
-      //   'avgEmployeeSatisfactionLevel',
-      //   'happyEmployees',
-      //   'unhappyEmployees'
-      // ];
+      let data = await db.collection('CompanyData').find({}).toArray()
+      let fields = []
 
+      if (data.length > 0) {
+        fields = Object.keys(data[0]).filter(elem => {
+          if (elem != '_id' || elem != 'insertionDate') { 
+            console.log("elem");
+            return true
+          }
+        })
+      }
 
-      const fields = ['hola', 'comoestas', 'adios', '_id', 'insertionDate']
-      const csv = json2csv({ data: data, fields: fields });
+      const csv = json2csv({ data, fields });
     
-      res.set("Content-Disposition", "attachment;filename=companydata.csv");
+      res.set("Content-Disposition", `attachment;filename=data${new Date()}.csv`);
       res.set("Content-Type", "application/octet-stream");
     
       res.send(csv);
@@ -94,24 +70,11 @@ module.exports = (app, db) => {
   })
 
   app.get('/template', (req, res) => {
-    const fields = [
-      'avgCompanyIncome',
-      'avgEmployeeIncome',
-      'company',
-      'companyType',
-      'itEmployees',
-      'totalItEmployees',
-      'jobPerks',
-      'officeCondition',
-      'avgEmployeeEmotionalStatus',
-      'avgEmployeeSatisfactionLevel',
-      'happyEmployees',
-      'unhappyEmployees'
-    ];
+    const fields = ['happy', 'neutral', 'unhappy'];
   
     const csv = json2csv({ data: '', fields: fields });
   
-    res.set("Content-Disposition", "attachment;filename=company-template.csv");
+    res.set("Content-Disposition", "attachment;filename=template.csv");
     res.set("Content-Type", "application/octet-stream");
   
     res.send(csv);
--- server.js
+++ server.js
@@ -3,11 +3,14 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const MongoClient = require('mongodb').MongoClient
+const socket = require('socket.io')
+const cors = require('cors')
 const db = require('./config/db')
 
 const app = express()
 const port = 5432
 
+app.use(cors())
 app.use(fileUpload())
 
 MongoClient.connect(db.url, (err, database) => {
